4dc6b5aeb5ea34ab6a62fd9e28a96c649b2d8b29
= <luiseduardo.0708@gmail.com>
2021-09-07 16:39:29 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developer by <a href="https://github.com/luiseduardom" target="_blank" rel="noreferrer">Luis Eduardo</a></p>
+                <p className="text-light"><small><strong>Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
